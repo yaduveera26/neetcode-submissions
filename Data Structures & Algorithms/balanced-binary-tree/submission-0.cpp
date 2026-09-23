65aee1e79465ccdf341d695aa26afd5983abf844
Yaduveera Kala <yaduveera26@gmail.com>
2026-09-23 14:28:32 +0530

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    bool isBalanced(TreeNode* root) {
+        if(!root) return true;
+       return abs(depth(root->left)-depth(root->right))<=1 ? true : false;
+    }
+
+    int depth(TreeNode* root){
+        if(!root) return 0;
+        return 1+max(depth(root->left),depth(root->right));
+    }
+};
